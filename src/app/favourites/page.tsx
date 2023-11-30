a6a97326e9c2002f3f76fa5baf473cd5900db0b9
Syed Raza Ali <itsrazaalishah303@gmail.com>
2023-11-30 11:49:54 -0700

    work on Favorite image page and functionality

--- src/app/favourites/page.tsx
+++ src/app/favourites/page.tsx
@@ -1,17 +1,13 @@
-import { CldImage } from "next-cloudinary"
-import UplaodButton from "./uplaod-button"
+
 import cloudinary from 'cloudinary'
-import { CloudinaryImage } from "./cloudinary-image"
+import { CloudinaryImage } from "../gallery/cloudinary-image"
+import { SearchResult } from "../gallery/page";
 
-export type SearchResult = {
-    public_id: string;
-    tags: string[]
 
-}
 
-export default async function GalleryPage() {
+export default async function FavoritesPage() {
     const result = (await cloudinary.v2.search
-        .expression('resource_type:image')
+        .expression('resource_type:image AND tags=favorite')
         .sort_by('created_at', 'desc')
         .with_field("tags")
         .max_results(30)
@@ -22,8 +18,8 @@ export default async function GalleryPage() {
         <section>
             <div className="flex flex-col gap-8">
                 <div className="flex justify-between">
-                    <h1 className="text-4xl font-bold">Gallery</h1>
-                    <UplaodButton /></div>
+                    <h1 className="text-4xl font-bold">Favorites Images</h1>
+                    </div>
                 <div className="grid grid-cols-4 gap-4">
                     {result.resources.map(result =>
                         <CloudinaryImage
--- src/app/gallery/cloudinary-image.tsx
+++ src/app/gallery/cloudinary-image.tsx
@@ -1,9 +1,40 @@
 "use client"
 
+import { Heart } from "@/components/ui/icons/heart"
 import { CldImage } from "next-cloudinary"
+import cloudinary from "cloudinary"
+import { setAsfavoriteAction } from "./action"
+import { useTransition } from "react"
+import { SearchResult } from "./page"
+import { FullHeart } from "@/components/ui/icons/ful-heart"
 
-export function CloudinaryImage (props : any){
+export function CloudinaryImage(props: any & {imageData: SearchResult}) {
+    const [transition, startTransition] = useTransition();
+    const {imageData} = props;
+    const isFavorited = imageData.tags.includes('favorite')
     return (
-        <CldImage {...props} />
+        <div className="relative">
+            <CldImage {...props} src={imageData.public_id} />
+            {
+                isFavorited ? (
+                <FullHeart
+                onClick={() => {
+                    startTransition(() => {
+                        setAsfavoriteAction(imageData.public_id, false)
+                    })
+                }}
+                className="absolute top-2 right-2 hover:text-white text-red-600 cursor-pointer" />
+                 ) : (
+                
+                <Heart
+                onClick={() => {
+                    startTransition(() => {
+                        setAsfavoriteAction(imageData.public_id, true )
+                    })
+                }}
+                className="absolute top-2 right-2 hover:text-red-500 cursor-pointer" />
+            )}
+            
+        </div>
     )
 }
